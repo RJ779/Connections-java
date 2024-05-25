bac8ad2bfa928e452f381838d62f1db74984f7f2
Riya Jain <124477951+RJ779@users.noreply.github.com>
2024-05-25 17:10:54 -0400

    Add files via upload

---
+++ MyProgram.java
@@ -0,0 +1,39 @@
+    import java.util.ArrayList;
+    import java.util.Scanner;
+    
+    public class MyProgram{
+        public static void main(String[] args) {
+            Scanner input= new Scanner(System.in);
+           
+            Connections trial = new Connections();
+
+            trial.printGame();
+            ArrayList<String[]> correctGuesses = new ArrayList<String[]>();  
+           
+            int errors=0;
+            String[] guesses = new String[4]; 
+            while(errors<4 && trial.getKey().size() != 0){
+                System.out.println("Type 4 words from the bank in order: ");
+                for(int i=1; i<5; i++){
+                    System.out.println("Enter guess " + i);
+                    guesses[i-1] = input.nextLine();  
+                }
+                if(trial.checkGuess(guesses)){
+                    correctGuesses.add(trial.getKey().get(trial.getIndexofRightGuess()));
+                    trial.removeFromKey(trial.getIndexofRightGuess());
+                    System.out.println("You're right!");
+                    trial.printGame();
+                }else{
+                    System.out.println("Try Again :)");
+                    errors++;
+                    trial.printGame();  
+                }
+                }
+                if(errors==4){
+                    System.out.println("Game Over");
+                }else{
+                    System.out.println("Congrats! You won!");
+                } 
+                input.close();
+        }
+    }
